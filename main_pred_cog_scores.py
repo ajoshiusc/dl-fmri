f5d5ad66403f058c8ed2de33df5c1058e30e1e96
Anand A Joshi <ajoshi@erdos.usc.edu>
2018-06-03 01:04:03 -0700

    update

--- main_pred_cog_scores.py
+++ main_pred_cog_scores.py
@@ -12,7 +12,8 @@ from DeepLearnfMRI import CogPredictor as cp
 
 def main():
     # Set name of Shark object
-    cp1 = cp.CogPredictor()
+    bfp_path = '/home/ajoshi/coding_ground/bfp'
+    cp1 = cp.CogPredictor(bfp_path)
     cp1.fit('hi11')
 #    cp.predict('hello')
 
