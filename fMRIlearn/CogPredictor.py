d730f5d9232f847fcd3848d59043a563eced7c64
Anand A Joshi <ajoshi@erdos.usc.edu>
2018-06-10 20:35:45 -0700

    update

--- fMRIlearn/CogPredictor.py
+++ fMRIlearn/CogPredictor.py
@@ -7,25 +7,31 @@ Created on Sun Jun  3 00:16:45 2018
 """
 import numpy as np
 import scipy as sp
+from sklearn.base import BaseEstimator
 
 # Define cognitive predictor regressor
 # This takes fMRI grayordinate data as input and cognitive scores as output
-class CogPredictor:
+class CogPredictor(BaseEstimator, bfp_path):
 
     def __init__(self, bfp_path):
+
         print("This is the constructor method.")
         # read the flat maps for both hemispheres
-        sqr_left = readdfs(bfp_path)
-        sqr_right = readdfs()
+        self.sqr_left = readdfs(bfp_path)
+        self.sqr_right = readdfs()
 
     def map_gord2sqrs(self, gord_data, sqr_size):
-        X, Y = sp.meshgrid(range(sqr_size), range(sqr_size))
+        X, Y = sp.meshgrid(np.linspace(-1.0, 1.0, sqr_size),
+                           np.linspace(-1.0, 1.0, sqr_size))
+        for t in range(gord_data.shape(1)):
+            
         left_data = 
         interp
 
     def fit(self, X, y):
         # X: data in grayordinates of shape Vert x Time x Subj
         # y: cognitive scores
+        map_gord2sqrs()
         print('Fitting the model')
         u_net = get_nn()
 
