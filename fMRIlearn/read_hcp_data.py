38ed1c3a2c1949c4271efb134df4b91b3861cea7
Anand A Joshi <ajoshi@erdos.usc.edu>
2018-06-16 03:07:24 -0700

    update

--- fMRIlearn/read_hcp_data.py
+++ fMRIlearn/read_hcp_data.py
@@ -7,14 +7,29 @@ Created on Sun Jun  3 00:16:45 2018
 """
 
 import nilearn as nl
+import os
 
 
 class HCPData():
-    def __init__(self, bfp_dir):
+    """ This class manages HCP data"""
+
+    def __init__(self, hcp_dir):
         # initialization
         self.rfmri = 0
         self.cog_scores = 0
+        self.hcp_dir = hcp_dir
         print("Read flat maps for left and right hemispheres.")
+
+    def get_data(self):
+        """get the rfMRI data"""
+        self.read_fMRI()
+    #    self.read_cog_scores(self.subids)
+        return self.rfmri, self.cog_scores
     
-    def get_rfmri()
-        return self.rfmri
+    def read_fMRI(self):
+        """ Read fMRI data from disk """
+        dirlst = os.listdir(self.hcp_dir)
+
+        for subid in dirlst:
+            print(subid)
+
--- main_pred_cog_scores.py
+++ main_pred_cog_scores.py
@@ -9,14 +9,19 @@ Created on Sun Jun  3 00:09:21 2018
 #||AUM||
 #||Shree Ganeshaya Namaha||
 import fMRIlearn.cog_predictor as cp
+import fMRIlearn.read_hcp_data as rh
 
 def main():
     """ Main script that calls the functions object"""
     bfp_path = '/home/ajoshi/coding_ground/bfp'
+    hcp_dir = '/deneb_disk/HCP'
+    xlspath = ''
     cp1 = cp.CogPred(bfp_path)
+    hpc = rh.HCPData(hcp_dir)
+    hpc.read_fMRI()
 
-    hcprfMRI = read_HCP_rfMRI(HCPpath)
-    hcpCogscores = read_HCP_cogscores(xlspath)
+#    hcprfMRI = read_hcp_rfMRI(HCPpath)
+#    hcpCogscores = read_hcp_cogscores(xlspath)
 #   cp1.fit(gordfiledir)
 #    cp.predict('hello')
 
