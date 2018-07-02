637708d7892cda72965633006a89415d9864ab82
Anand A Joshi <ajoshi@erdos.usc.edu>
2018-07-02 00:30:10 -0700

    update

--- fMRIlearn/cog_predictor.py
+++ fMRIlearn/cog_predictor.py
@@ -95,9 +95,11 @@ class CogPred(BaseEstimator, bfpData):
 #        y=np.array([11,12,13,14,15]).reshape((5,1))
         y = self.cog_scores['ADHD Index'][self.subids].get_values()
 
-        X = X[y !=-999, :, :, :]
+        X[0] = X[0][y !=-999, :, :, :]
+        X[1] = X[1][y !=-999, :, :, :]
+        X[2] = X[2][y !=-999, :, :]
         y = y[y !=-999]
-        y = y[None,:]
+        y = y[None, :]
 
 
         history = self.hybrid_cnn.fit(X, y, batch_size=5, epochs=20, verbose=1,
