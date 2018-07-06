98033e9f1ebbf4126c0822be83f2dee0fe494350
Anand A Joshi <ajoshi@erdos.usc.edu>
2018-07-06 12:51:01 -0700

    Replaced the NN with VGG style model.

--- fMRIlearn/cog_predictor.py
+++ fMRIlearn/cog_predictor.py
@@ -11,8 +11,9 @@ import itertools
 import numpy as np
 from scipy.io import loadmat
 from scipy.interpolate import griddata
-from keras.layers import Input, Conv2D, concatenate, MaxPooling2D, Flatten, Dense
+from keras.layers import Input, Conv2D, concatenate, MaxPooling2D, Flatten, Dense, ZeroPadding2D, Dropout
 from keras.models import Model
+from keras.optimizers import SGD
 from keras.callbacks import ModelCheckpoint
 from keras import backend as K
 from keras import losses
@@ -43,8 +44,8 @@ class CogPred(BaseEstimator, bfpData):
         self.nn_ipdata = list()
         print("Read flat maps for left and right hemispheres.")
         self.hybrid_cnn = self.get_neural_net()
-        self.ref_subno = 0
-        self.ref_data = 0
+        self.ref_subno = None
+        self.ref_data = None
 
     def map_gord2sqrs(self, sqr_size=256):
         """This function maps grayordinate data to square
@@ -118,7 +119,9 @@ class CogPred(BaseEstimator, bfpData):
 
         # Save the reference subject and ref subject data
         np.savez_compressed(
-            'Refdata.npz', ref_data=self.ref_data, ref_subno=self.ref_subno)
+            'Refdata_test.npz',
+            ref_data=self.ref_data,
+            ref_subno=self.ref_subno)
 
         print('The most representative subject is %d' % self.ref_subno)
 
@@ -128,7 +131,7 @@ class CogPred(BaseEstimator, bfpData):
             print(
                 '=======\n Reference subject is not initialized, loading from a file\n=======\n'
             )
-            a = np.load('Refdata.npz')
+            a = np.load('Refdata_test.npz')
             self.ref_subno = a['ref_subno']
             self.ref_data = a['ref_data']
 
@@ -153,32 +156,38 @@ class CogPred(BaseEstimator, bfpData):
         self.map_gord2sqrs()
 
         model_checkpoint = ModelCheckpoint(
-            'weights3d.h5', monitor='val_loss', save_best_only=True)
+            'weights3d_test.h5', monitor='val_loss', save_best_only=True)
 
         X = self.nn_ipdata
         #        y=np.array([11,12,13,14,15]).reshape((5,1))
         y = self.cog_scores['ADHD Index'][self.subids].get_values()
 
-        X[0] = X[0][y != -999, :, :, :]
-        X[1] = X[1][y != -999, :, :, :]
-        X[2] = X[2][y != -999, :, :]
-        y = y[y != -999]
+        X[0] = X[0][y > 0, :, :, :]
+        X[1] = X[1][y > 0, :, :, :]
+        X[2] = X[2][y > 0, :, :]
+        y = y[y > 0] / 50.0
         # y = y[:]
+        X = X[0].astype('float32')
+        y = y.astype('float32')
 
-        print('Number of subjects: %d\n' % (y.shape[0]))
+        print('training with this data\n')
+        print(y)
+        print(X)
+
+        #        print('Number of subjects: %d\n' % (y.shape[0]))
 
         history = self.hybrid_cnn.fit(
             X,
             y,
-            batch_size=1,
-            epochs=2,
+            batch_size=10,
+            epochs=20,
             verbose=1,
             shuffle=True,
             validation_split=0.2,
             callbacks=[model_checkpoint])
 
         print('=======\nSaving training history\n=======')
-        with open('trainHistoryDict_test', 'wb') as file_pi:
+        with open('trainHistoryDict', 'wb') as file_pi:
             pickle.dump(history.history, file_pi)
 
         print('=======\nDisplaying training history\n=======')
@@ -191,79 +200,86 @@ class CogPred(BaseEstimator, bfpData):
         plt.xlabel('epoch')
         plt.legend(['train', 'val'], loc='upper left')
         plt.show()
-        # summarize history for loss
-        plt.plot(history.history['loss'])
-        plt.plot(history.history['val_loss'])
-        plt.title('model loss')
-        plt.ylabel('loss')
-        plt.xlabel('epoch')
-        plt.legend(['train', 'val'], loc='upper left')
-        plt.show()
 
     def predict(self, data_dir, csv_file):
 
         mod = self.get_neural_net()
-
-        mod.load_weights('weights3d.h5')
+        mod.load_weights('weights3d_test.h5')
 
         self.read_fmri(data_dir, reduce_dim=21)
         self.sync2rep()
         self.read_cog_scores(csv_file)
         self.map_gord2sqrs()
-        X = self.nn_ipdata
-        y = self.cog_scores['ADHD Index'][self.subids].get_values()
-
+        X = self.nn_ipdata[0].astype('float32')
+        y = self.cog_scores['ADHD Index'][self.subids].get_values() / 50.0
+        y = y.astype('float32')
         ypred = mod.predict(X, verbose=1)
 
         return y, ypred
 
     def get_neural_net(self, isize=[256, 256]):
-        input_lh = Input((isize[0], isize[1], 21))
-        input_rh = Input((isize[0], isize[1], 21))
-        sub_cort = Input((self.nvert_subcort, 21))
+        """VGG model with one FC layer added at the end for continuous output"""
 
-        conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(input_lh)
-        conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(input_rh)
-        pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-        d1_ip = Dense(512, activation='relu')(sub_cort)
-        d1_ip = Flatten()(d1_ip)
-        d1_ip = Dense(1, activation='relu')(d1_ip)
+        img_rows = isize[0]
+        img_cols = isize[1]
+        #VGG model
+        main_input = Input(
+            shape=(isize[0], isize[1], 21), dtype='float32', name='main_input')
+        zpad1 = ZeroPadding2D(
+            (1, 1), input_shape=(isize[0], isize[1], 21),
+            name='zpad1')(main_input)
+        conv1 = Conv2D(64, (3, 3), name='conv1', activation='relu')(zpad1)
+        zpad2 = ZeroPadding2D((1, 1), name='zpad2')(conv1)
+        conv2 = Conv2D(64, (3, 3), name='conv2', activation='relu')(zpad2)
+        maxp1 = MaxPooling2D((2, 2), strides=(2, 2), name='maxp1')(conv2)
 
-        conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
-        conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
-        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+        zpad3 = ZeroPadding2D((1, 1), name='zpad3')(maxp1)
+        conv3 = Conv2D(128, (3, 3), name='conv3', activation='relu')(zpad3)
+        zpad4 = ZeroPadding2D((1, 1), name='zpad4')(conv3)
+        conv4 = Conv2D(128, (3, 3), name='conv4', activation='relu')(zpad4)
+        maxp2 = MaxPooling2D((2, 2), strides=(2, 2), name='maxp2')(conv4)
 
-        conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
-        conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
-        pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+        zpad5 = ZeroPadding2D((1, 1), name='zpad5')(maxp2)
+        conv5 = Conv2D(256, (3, 3), name='conv5', activation='relu')(zpad5)
+        zpad6 = ZeroPadding2D((1, 1), name='zpad6')(conv5)
+        conv6 = Conv2D(256, (3, 3), name='conv6', activation='relu')(zpad6)
+        zpad7 = ZeroPadding2D((1, 1), name='zpad7')(conv6)
+        conv7 = Conv2D(256, (3, 3), name='conv7', activation='relu')(zpad7)
+        maxp3 = MaxPooling2D((2, 2), strides=(2, 2), name='maxp3')(conv7)
 
-        conv4_1 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
-        conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4_1)
-        conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
-        pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+        zpad8 = ZeroPadding2D((1, 1), name='zpad8')(maxp3)
+        conv8 = Conv2D(512, (3, 3), name='conv8', activation='relu')(zpad8)
+        zpad9 = ZeroPadding2D((1, 1), name='zpad9')(conv8)
+        conv9 = Conv2D(512, (3, 3), name='conv9', activation='relu')(zpad9)
+        zpad10 = ZeroPadding2D((1, 1), name='zpad10')(conv9)
+        conv10 = Conv2D(512, (3, 3), name='conv10', activation='relu')(zpad10)
+        maxp4 = MaxPooling2D((2, 2), strides=(2, 2), name='maxp4')(conv10)
 
-        conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
-        conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
-        flat1 = Flatten()(conv5)
+        zpad11 = ZeroPadding2D((1, 1), name='zpad11')(maxp4)
+        conv11 = Conv2D(512, (3, 3), name='conv11', activation='relu')(zpad11)
+        zpad12 = ZeroPadding2D((1, 1), name='zpad12')(conv11)
+        conv12 = Conv2D(512, (3, 3), name='conv12', activation='relu')(zpad12)
+        zpad13 = ZeroPadding2D((1, 1), name='zpad13')(conv12)
+        conv13 = Conv2D(512, (3, 3), name='conv13', activation='relu')(zpad13)
+        maxp5 = MaxPooling2D((2, 2), strides=(2, 2), name='maxp5')(conv13)
 
-        d1 = Dense(512, activation='relu')(flat1)
-        d2 = Dense(1, activation='relu')(d1)
-        d2 = concatenate([d2, d1_ip], axis=-1)
+        flatten = Flatten(name='flatten')(maxp5)
+        dense1 = Dense(4096, activation='relu', name='dense1')(flatten)
+        dropout1 = Dropout(0.5, name='dropout1')(dense1)
+        dense2 = Dense(4096, activation='relu', name='dense2')(dropout1)
+        dropout2 = Dropout(0.5, name='dropout2')(dense2)
+        dense3 = Dense(1000, activation='relu', name='dense3')(dropout2)
+        dropout3 = Dropout(0.5, name='dropout3')(dense3)
+        dense4 = Dense(256, activation='relu', name='dense4')(dropout3)
 
-        out_theta = Dense(1)(d2)
-        #    conv_tx = Conv2D(1, (1, 1), activation=final_activation)(conv5)
-        #    conv_ty = Conv2D(1, (1, 1), activation=final_activation)(conv5)
-        #    conv_theta = Conv2D(1, (1, 1), activation='tanh')(conv5)
+        # My addition of regression layer
+        out_theta = Dense(1, activation='relu')(dense4)
+        print("==Defining Model  ==")
+        model = Model(inputs=[main_input], outputs=[out_theta])
 
-        #    out_img = rotate(inputs,conv_theta)
-
-        #        model = Model(inputs=[input_lh, input_rh, sub_cort], outputs=out_theta)
-        model = Model(inputs=[input_lh, input_rh, sub_cort], outputs=out_theta)
-
+        print("==Compiling Model ==")
+        sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
         model.compile(
-            optimizer='adam', loss=losses.mean_squared_error, metrics=['mse'])
+            optimizer=sgd, loss=losses.mean_squared_error, metrics=['mse'])
 
         return model
-
-
-# fdfdh
